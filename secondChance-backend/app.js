da85ae0b937b6a438d68246668360b23bc727fcc
jotamendi <juacom18@hotmail.com>
2025-11-21 11:34:06 -0500

    Second Chance items service

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -26,8 +26,8 @@ app.use(express.json());
 // authRoutes Step 2: import the authRoutes and store in a constant called authRoutes
 //{{insert code here}}
 
-// Items API Task 1: import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
-//{{insert code here}}
+// import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
+const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
@@ -42,8 +42,8 @@ app.use(pinoHttp({ logger }));
 // authRoutes Step 2: add the authRoutes and to the server by using the app.use() method.
 //{{insert code here}}
 
-// Items API Task 2: add the secondChanceItemsRoutes to the server by using the app.use() method.
-//{{insert code here}}
+// add the secondChanceItemsRoutes to the server by using the app.use() method.
+app.use('/api/secondchance/items', secondChanceItemsRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
--- secondChance-backend/models/db.js
+++ secondChance-backend/models/db.js
@@ -12,17 +12,17 @@ async function connectToDatabase() {
     if (dbInstance){
         return dbInstance
     };
-
+    
     const client = new MongoClient(url);      
 
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
+    // Connect to MongoDB
+    await client.connect()
 
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
+    // Connect to database giftDB and store in variable dbInstance
+    dbInstance = client.db(dbName);
 
-    // Task 3: Return database instance
-    // {{insert code}}
+    // Return database instance
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
