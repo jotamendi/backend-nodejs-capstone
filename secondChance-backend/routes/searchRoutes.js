6aebc35e8c95ea107d769141a72e66cb21351b16
jotamendi <48560133+jotamendi@users.noreply.github.com>
2025-11-26 18:26:52 -0300

    Refactor query initialization in searchRoutes.js

--- secondChance-backend/routes/searchRoutes.js
+++ secondChance-backend/routes/searchRoutes.js
@@ -9,20 +9,20 @@ router.get('/', async (req, res, next) => {
     const db = await connectToDatabase()
     const collection = db.collection(process.env.MONGO_COLLECTION)
     // Initialize the query object
-    let query = {}
+    const query = {}
     // Add the name filter to the query if the name parameter is not empty
     if (req.query.name && req.query.name.trim() !== '') {
-        query.name = { $regex: req.query.name, $options: 'i' } // Using regex for partial match, case-insensitive
+      query.name = { $regex: req.query.name, $options: 'i' } // Using regex for partial match, case-insensitive
     }
     // Task 3: Add other filters to the query
     if (req.query.category) {
-        query.category = req.query.category
+      query.category = req.query.category
     }
     if (req.query.condition) {
-        query.condition = req.query.condition
+      query.condition = req.query.condition
     }
     if (req.query.age_years) {
-        query.age_years = { $lte: parseInt(req.query.age_years) }
+      query.age_years = { $lte: parseInt(req.query.age_years) }
     }
     // Fetch filtered gifts using the find(query) method.
     const gifts = await collection.find(query).toArray()
